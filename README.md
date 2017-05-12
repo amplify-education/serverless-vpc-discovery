e6ac1b24cc43f828c3ee0bc5fd32c60b8156aa7e
Johnathan Constance <jconstance@amplify.com>
2017-05-12 09:49:31 -0400

    Initial Commit / Open Source
    Squash all previous history. Up to this point all development done at
    Amplify Inc. with individual contributions from:
    
    * wongJonathan          90 commits / 3,092 ++ / 2,442 --
    * aoskotsky-amplify     5 commits / 279 ++ / 364 --
    * jconstance-amplify    1 commit / 4 ++ / 6 --

---
+++ README.md
@@ -0,0 +1,83 @@
+# serverless-vpc-discovery
+
+The vpc discovery plugin takes the given vpc, subnet, and security group names in the serverless file to setup the vpc configuration for the lambda.
+
+# About Amplify
+Amplify builds innovative and compelling digital educational products that empower teachers and students across the country. We have a long history as the leading innovator in K-12 education - and have been described as the best tech company in education and the best education company in tech. While others try to shrink the learning experience into the technology, we use technology to expand what is possible in real classrooms with real students and teachers.
+
+Learn more at https://www.amplify.com
+
+# Getting Started
+
+## Prerequisites
+Make sure you have the following installed before starting:
+* [nodejs](https://nodejs.org/en/download/)
+* [npm](https://www.npmjs.com/get-npm?utm_source=house&utm_medium=homepage&utm_campaign=free%20orgs&utm_term=Install%20npm)
+* [serverless](https://serverless.com/framework/docs/providers/aws/guide/installation/)
+
+Also allow the lambda to have the following IAM permissions:
+* ec2:CreateNetworkInterface
+* ec2:DescribeNetworkInterfaces
+* ec2:DeleteNetworkInterface
+
+## Installation
+Run:
+```
+# From npm (recommended)
+npm install serverless-vpc-discovery
+
+# From github
+npm install https://github.com/amplify-education/serverless-vpc-discovery.git
+```
+Then make the following edits to your serverless.yaml file:
+```yaml
+plugins:
+  - serverless-vpc-discovery
+
+custom:
+  vpc:
+    vpcName: '${opt:env}'
+    subnetNames:
+      - '${opt:env}_NAME OF SUBNET'
+    securityGroupNames:
+      - '${opt:env}_NAME OF SECURITY GROUP'
+```
+> NOTE: The naming pattern we used here was building off the vpc name for the subnet and security group by extending it with the the subnet and security group name. This makes it easier to switch to different vpcs by changing the environment variable in the command line
+
+## Running Tests
+To run the test:
+```
+npm test
+```
+All tests should pass.
+
+If there is an error update the node_module inside the serverless-vpc-discovery folder:
+```
+npm install
+```
+
+## Deploying with the plugin
+When deploying run:
+```
+serverless deploy --env 'VPC Name'
+```
+
+And that should be it! Good Luck!
+
+# How it Works
+
+The vpc, subnets, and security groups are found by filtering based on a specified tag name.
+Vpc and subnets are found under the tag name `tag:Name`.
+Security groups are found by the name of the group under `group-name`.
+
+The vpc is found first as it is used to find the subnets and security groups. Once all of the subnets and security groups are found the serverless service provider creates a vpc object and stores the subnets and security groups.
+
+# Responsible Disclosure
+If you have any security issue to report, contact project maintainers privately.
+You can reach us at <github@amplify.com>
+
+# Contributing
+We welcome pull requests! For your pull request to be accepted smoothly, we suggest that you:
+1. For any sizable change, first open a GitHub issue to discuss your idea.
+2. Create a pull request.  Explain why you want to make the change and what it’s for.
+We’ll try to answer any PR’s promptly.
---
+++ index.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const _ = require('underscore');
+
+class VPCPlugin {
+  constructor(serverless) {
+    this.serverless = serverless;
+    AWS.config.update({
+      region: serverless.service.provider.region,
+    });
+    this.ec2 = new AWS.EC2();
+
+    /* hooks are the acutal code that will run when called */
+    this.hooks = {
+      'before:deploy:initialize': this.updateVpcConfig.bind(this),
+    };
+  }
+
+  /**
+   *  Returns the promise that contains the vpc-id
+   * @param {string} vpcName
+   * @returns {Promise.<string>}
+   */
+  getVpcId(vpcName) {
+    const vpcParams = {
+      Filters: [{
+        Name: 'tag:Name',
+        Values: [vpcName],
+      }],
+    };
+
+    return this.ec2.describeVpcs(vpcParams).promise().then((data) => {
+      // If it cannot find a vpc, vpc does not exist for that name
+      if (data.Vpcs.length === 0) {
+        throw new Error('Invalid vpc name, it does not exist');
+      }
+      return data.Vpcs[0].VpcId;
+    });
+  }
+
+  /**
+   * Returns the promise that contains the subnet IDs
+   *
+   * @param {string} vpcId
+   * @param {string[]} subnetNames
+   * @returns {Promise.<string[]>}
+   */
+  getSubnetIds(vpcId, subnetNames) {
+    const paramsSubnet = {
+      Filters: [{
+        Name: 'vpc-id',
+        Values: [vpcId],
+      }, {
+        Name: 'tag:Name',
+        Values: subnetNames,
+      }],
+    };
+
+    return this.ec2.describeSubnets(paramsSubnet).promise().then((data) => {
+      if (data.Subnets.length === 0) {
+        throw new Error('Invalid subnet name, it does not exist');
+      }
+
+      if (paramsSubnet.Filters[1].Values.length !== data.Subnets.length) {
+        // Creates a list of the valid subnets
+        const validSubnets = data.Subnets.reduce((accum, val) => {
+          const nameTag = val.Tags.find(tag => tag.Key === 'Name');
+
+          if (nameTag) {
+            accum.push(nameTag.Value);
+          }
+
+          return accum;
+        }, []);
+        // Compares the valid subents with ones given to find invalid subnet names
+        const missingSubnets = _.difference(paramsSubnet.Filters[1].Values, validSubnets);
+
+        throw new Error(`Not all subnets were registered: ${missingSubnets}`);
+      }
+      const subnetIds = data.Subnets.map(obj => obj.SubnetId);
+
+      return subnetIds;
+    });
+  }
+
+
+  /**
+   *  Returns the promise that contains the security group IDs
+   * @param {string} vpcId
+   * @param {string[]} securityGroupNames
+   * @returns {Promise.<string[]>}
+   */
+  getSecurityGroupIds(vpcId, securityGroupNames) {
+    const paramsSecurity = {
+      Filters: [{
+        Name: 'vpc-id',
+        Values: [vpcId],
+      }, {
+        Name: 'group-name',
+        Values: securityGroupNames,
+      }],
+
+    };
+
+    return this.ec2.describeSecurityGroups(paramsSecurity).promise().then((data) => {
+      if (data.SecurityGroups.length === 0) {
+        throw new Error('Invalid security group name, it does not exist');
+      }
+
+      if (paramsSecurity.Filters[1].Values.length !== data.SecurityGroups.length) {
+        const validGroups = data.SecurityGroups.map(obj => obj.GroupName);
+        const missingGroups = _.difference(paramsSecurity.Filters[1].Values, validGroups);
+        throw new Error(`Not all security group were registered: ${missingGroups}`);
+      }
+
+      const securityGroupIds = data.SecurityGroups.map(obj => obj.GroupId);
+
+      return securityGroupIds;
+    });
+  }
+
+  /**
+   * Gets the desired vpc with the designated subnets and security groups
+   * that were set in serverless config file
+   * @returns {Promise}
+   */
+  updateVpcConfig() {
+    this.serverless.cli.log('Updating VPC config...');
+    const service = this.serverless.service;
+
+    // Checks if the serverless file is setup correctly
+    if (service.custom.vpc.vpcName == null || service.custom.vpc.subnetNames == null ||
+      service.custom.vpc.securityGroupNames == null) {
+      throw new Error('Serverless file is not configured correctly. Please see README for proper setup.');
+    }
+
+
+    // Returns the vpc with subnet and security group ids
+    return this.getVpcId(service.custom.vpc.vpcName).then((vpcId) => {
+      const promises = [
+        this.getSubnetIds(vpcId, service.custom.vpc.subnetNames),
+        this.getSecurityGroupIds(vpcId, service.custom.vpc.securityGroupNames),
+      ];
+
+      return (Promise.all(promises).then((values) => {
+        // Checks to see if either subnets or security gropus returned nothing
+        if (!values[0].length || !values[1].length) {
+          throw new Error('Vpc was not set');
+        }
+
+        // Sets the serverless's vpc config
+        service.provider.vpc = {
+          subnetIds: values[0],
+          securityGroupIds: values[1],
+        };
+
+        return service.provider.vpc;
+      }));
+    }).catch((err) => {
+      throw new Error(`Could not set vpc config. Message: ${err}`);
+    });
+  }
+}
+module.exports = VPCPlugin;
