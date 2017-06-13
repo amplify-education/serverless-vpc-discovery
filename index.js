abd041f7105d4d1a0042f2c1b964b4e0e6da176a
jwong <jwong@amplify.com>
2017-06-13 10:45:01 -0400

    Fixed eslinting error

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class VPCPlugin {
   updateVpcConfig() {
     const awsCreds = this.serverless.providers.aws.getCredentials();
 
-    this.ec2 = new AWS.EC2({credentials: awsCreds});
+    this.ec2 = new AWS.EC2({ credentials: awsCreds });
 
     this.serverless.cli.log('Updating VPC config...');
     const service = this.serverless.service;
